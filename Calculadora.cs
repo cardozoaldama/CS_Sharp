055252bb009762302ec5031173dc12ed044c3971
Fernando Cardozo <nuevo_10000@outlook.com>
2024-11-25 10:06:57 -0300

    feat: menú para el programa principal

--- Calculadora.cs
+++ Calculadora.cs
@@ -3,6 +3,33 @@ using System
 class Calculadora{
 
     static void Main(){
-        Console.WriteLine("Hola mundo");
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Calculadora de Consola");
+            Console.WriteLine("1. Suma");
+            Console.WriteLine("2. Factorial");
+            Console.WriteLine("3. Salir");
+            Console.Write("Selecciona una opción: ");
+            string opcion = Console.ReadLine();
+            switch (opcion)
+            {
+            case "1":
+                RealizarSuma();
+                break;
+            case "2":
+                CalcularFactorial();
+                break;
+            case "3":
+                Console.WriteLine("Hasta luego");
+                return;
+            default:
+                Console.WriteLine("Opción no válida. Presiona cualquier tecla para intentarlo de nuevo.");
+                Console.ReadKey();
+                break;
+            }
+
+        }
     }
 }
